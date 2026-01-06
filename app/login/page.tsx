bb5e28a919d2fbbcc6f9b16dd8abb04b1fef694b
galancstas-prog <galancstas@gmail.com>
2026-01-06 22:31:45 +0500

    Updated page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+console.log('LOGIN HANDLER CALLED')
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createBrowserClient } from '@supabase/ssr'
