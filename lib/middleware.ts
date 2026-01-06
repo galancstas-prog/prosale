3b398b54c272ef2e671f6b95148f03bf228cadc8
galancstas-prog <galancstas@gmail.com>
2026-01-06 17:25:16 +0500

    Updated middleware.ts

---
+++ lib/middleware.ts
@@ -0,0 +1,43 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options)
+          })
+        },
+      },
+    }
+  )
+
+  // ❗ ОБЯЗАТЕЛЬНО
+  await supabase.auth.getUser()
+
+  return response
+}
+
+export const config = {
+  matcher: [
+    /*
+      Пропускаем статические файлы и api, всё остальное — через middleware
+    */
+    '/((?!_next/static|_next/image|favicon.ico|api/auth).*)',
+  ],
+}
