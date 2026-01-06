8800adb5f98c6797eccdb2b89480c1d4d63d836e
galancstas-prog <galancstas@gmail.com>
2026-01-06 16:50:06 +0500

    Updated supabase-client.ts

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -3,9 +3,4 @@ import { createBrowserClient } from '@supabase/ssr'
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)import { createBrowserClient } from '@supabase/ssr'
-
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
